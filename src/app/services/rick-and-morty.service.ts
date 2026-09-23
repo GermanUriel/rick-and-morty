7e17b3d2089316b69b517aaf8a3fa2fda1fab2df
Man <german24491@cbtis75.edu.mx>
2026-09-23 07:42:35 -0600

    prueba
    si

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -62,15 +62,7 @@ export class RickAndMortyService {
     );
   }
 
-  // 7. Obtener todos los episodios
-  obtenerEpisodios(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/episode`);
-  }
 
-  // 8. Obtener un episodio por ID
-  obtenerEpisodio(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/episode/${id}`);
-  }
 
   // 9. Obtener todas las ubicaciones
   obtenerUbicaciones(): Observable<any> {
